ca42315bec050097e6d33849f822220adc98573a
rceglie <rceglie@ad.unc.edu>
2021-04-22 22:47:38 -0400

    added files

--- src/main/java/com/comp301/a09nonograms/model/BoardImpl.java
+++ src/main/java/com/comp301/a09nonograms/model/BoardImpl.java
@@ -18,7 +18,7 @@ public class BoardImpl implements Board{
     }
 
     public boolean isShaded(int row, int col) {
-        System.out.println("Row: " + row + ", Col: " + col);
+        System.out.println("[" + row + ", " + col + "]");
         if (row >= height || row < 0 || col < 0 || col >= width){
             throw new RuntimeException();
         }
